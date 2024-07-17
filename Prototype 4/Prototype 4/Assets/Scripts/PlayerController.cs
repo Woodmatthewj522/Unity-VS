88133b9fee3632d62e4515353d2ce686cbca0023
Matthew Wood <Woodmatthewj522@gmail.com>
2024-07-17 13:20:29 -0400

    Prototype 4

--- Prototype 4/Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Prototype 4/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    private float powerUpStrength = 15.0f;
     public float speed = 5.0f;
+    public bool powerPotion = false;
+    public GameObject powerUpIndicator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,36 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerPotion"))
+        {
+            powerPotion = true;
+            powerUpIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCDRoutine());
+        }
+    }
+
+    IEnumerator PowerUpCDRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        powerPotion = false;
+        powerUpIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && powerPotion)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("Collided with : " + collision.gameObject.name + " with power-up set to " + powerPotion);
+        }
     }
 }
